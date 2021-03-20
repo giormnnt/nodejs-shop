81caacd4e759d1062e9b5439dbb95020011e79cd
k1yeon <gio13remanente@gmail.com>
2021-03-20 13:57:23 +0800

    Refactored: created a helper function

--- models/product.js
+++ models/product.js
@@ -1,7 +1,20 @@
 const fs = require('fs');
 const path = require('path');
 
-const dir = path.dirname(process.mainModule.filename);
+const p = path.join(
+  path.dirname(process.mainModule.filename),
+  'data',
+  'products.json'
+);
+
+const getProductsFromFile = cb => {
+  fs.readFile(p, (err, fileContent) => {
+    if (err) {
+      return cb([]);
+    }
+    cb(JSON.parse(fileContent));
+  });
+};
 
 module.exports = class Product {
   constructor(title) {
@@ -9,12 +22,7 @@ module.exports = class Product {
   }
 
   save() {
-    const p = path.join(dir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      let products = [];
-      if (!err) {
-        products = JSON.parse(fileContent);
-      }
+    getProductsFromFile(products => {
       products.push(this);
       fs.writeFile(p, JSON.stringify(products), err => {
         console.log(err);
@@ -23,12 +31,6 @@ module.exports = class Product {
   }
 
   static fetchAll(cb) {
-    const p = path.join(dir, 'data', 'products.json');
-    fs.readFile(p, (err, fileContent) => {
-      if (err) {
-        cb([]);
-      }
-      cb(JSON.parse(fileContent));
-    });
+    getProductsFromFile(cb);
   }
 };
