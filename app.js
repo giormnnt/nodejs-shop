e5a30e0bf307ad31276851b0d8f4ce02d2a7d861
k1yeon <gio13remanente@gmail.com>
2021-03-20 11:08:43 +0800

    Changed variable for clarity

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const path = require('path');
 const express = require('express');
 const bodyParser = require('body-parser');
 
-const err = require('./controllers/404');
+const errorController = require('./controllers/404');
 
 const app = express();
 
@@ -18,6 +18,5 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
-app.use(err.get404);
-
+app.use(errorController.get404);
 app.listen(3000);
