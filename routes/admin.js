6934acf9bd199d5d87eafa33a6d92431014d9cd9
k1yeon <gio13remanente@gmail.com>
2021-03-20 09:57:55 +0800

    Initial commit

---
+++ routes/admin.js
@@ -0,0 +1,26 @@
+const path = require('path');
+
+const express = require('express');
+
+const rootDir = require('../util/path');
+
+const router = express.Router();
+
+const products = [];
+
+router.get('/add-product', (req, res, next) => {
+  // * same syntax with any template engines
+  res.render('add-product', {
+    products,
+    pageTitle: 'Add Product',
+    path: '/admin/add-product',
+  });
+});
+
+router.post('/add-product', (req, res, next) => {
+  products.push({ title: req.body.title });
+  res.redirect('/');
+});
+
+exports.routes = router;
+exports.products = products;
