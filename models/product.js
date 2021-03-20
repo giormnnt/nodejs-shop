e9f1d112f8bc412888403d022a6fe62f601173e5
k1yeon <gio13remanente@gmail.com>
2021-03-20 13:41:15 +0800

    File storage added for temporary database

--- models/product.js
+++ models/product.js
@@ -1,4 +1,7 @@
-const products = [];
+const fs = require('fs');
+const path = require('path');
+
+const dir = path.dirname(process.mainModule.filename);
 
 module.exports = class Product {
   constructor(title) {
@@ -6,10 +9,26 @@ module.exports = class Product {
   }
 
   save() {
-    products.push(this);
+    const p = path.join(dir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      let products = [];
+      if (!err) {
+        products = JSON.parse(fileContent);
+      }
+      products.push(this);
+      fs.writeFile(p, JSON.stringify(products), err => {
+        console.log(err);
+      });
+    });
   }
 
-  static fetchAll() {
-    return products;
+  static fetchAll(cb) {
+    const p = path.join(dir, 'data', 'products.json');
+    fs.readFile(p, (err, fileContent) => {
+      if (err) {
+        cb([]);
+      }
+      cb(JSON.parse(fileContent));
+    });
   }
 };
