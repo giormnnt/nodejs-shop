ab62765021d4652e9b5e0fac32441203f9c4a921
k1yeon <gio13remanente@gmail.com>
2021-04-04 05:57:55 +0800

    fix: redirecting to cart page after adding item to cart

--- controllers/shop.js
+++ controllers/shop.js
@@ -54,36 +54,9 @@ exports.postCart = (req, res, next) => {
   const { productId } = req.body;
   Product.findById(productId)
     .then(product => {
-      return req.user.addToCart(product);
+      req.user.addToCart(product);
     })
-    .then(result => {
-      console.log(result);
-    });
-  // let fetchedCart;
-  // let newQuantity = 1;
-  // req.user
-  //   .getCart()
-  //   .then(cart => {
-  //     fetchedCart = cart;
-  //     return cart.getProducts({ where: { id: productId } });
-  //   })
-  //   .then(products => {
-  //     let product;
-  //     if (products.length > 0) product = products[0];
-  //     if (product) {
-  //       const oldQuantity = product.cartItem.quantity;
-  //       newQuantity = oldQuantity + 1;
-  //       return product;
-  //     }
-  //     return Product.findByPk(productId);
-  //   })
-  //   .then(product => {
-  //     return fetchedCart.addProduct(product, {
-  //       through: { quantity: newQuantity },
-  //     });
-  //   })
-  //   .then(() => res.redirect('/cart'))
-  //   .catch(err => console.log(err));
+    .then(() => res.redirect('/cart'));
 };
 
 exports.postCartDeleteProduct = (req, res, next) => {
