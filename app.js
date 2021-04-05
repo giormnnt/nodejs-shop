8c8474d50428c50655b092ef9e5cc2f96fa08bf6
k1yeon <gio13remanente@gmail.com>
2021-04-05 09:35:49 +0800

    adding and using user model

--- app.js
+++ app.js
@@ -5,8 +5,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 
 const errorController = require('./controllers/404');
-// const { mongoConnect } = require('./util/database');
-// const User = require('./models/user');
+const User = require('./models/user');
 
 const app = express();
 
@@ -18,14 +17,15 @@ const shopRoutes = require('./routes/shop');
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, 'public')));
 
-// app.use((req, res, next) => {
-//   User.findById('60664b79a26c13704ff69268')
-//     .then(user => {
-//       req.user = new User(user.name, user.email, user.cart, user._id);
-//       next();
-//     })
-//     .catch(err => console.log(err));
-// });
+app.use((req, res, next) => {
+  User.findById('606a66c4ebb3a60f9881a092')
+    .then(user => {
+      console.log('helo');
+      req.user = user; // * stores user model in request
+      next();
+    })
+    .catch(err => console.log(err));
+});
 
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -37,7 +37,22 @@ mongoose
     'mongodb+srv://Giovanni:npsssYf5cEvNEhRb@cluster0.dxeqa.mongodb.net/shop?retryWrites=true&w=majority',
     { useNewUrlParser: true, useUnifiedTopology: true }
   )
-  .then(result => {
+  .then(() => {
+    // * creates new user if there's NO user.
+    // * returns the first user it finds
+    User.findOne().then(user => {
+      if (!user) {
+        const user = new User({
+          name: 'Gio',
+          email: 'gio@test.com',
+          cart: {
+            items: [],
+          },
+        });
+        user.save();
+      }
+    });
+
     app.listen(3000);
   })
   .catch(err => console.log(err));
