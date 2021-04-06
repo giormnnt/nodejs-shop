e3c5f422178c6b70bb2ccc089dfd9d9a82bb0cb4
k1yeon <gio13remanente@gmail.com>
2021-04-07 05:11:37 +0800

    feat: clearing cart after checking out the order

--- models/user.js
+++ models/user.js
@@ -62,6 +62,11 @@ userSchema.methods.deleteFromCart = function (productId) {
   return this.save();
 };
 
+userSchema.methods.clearCart = function () {
+  this.cart = { items: [] };
+  return this.save();
+};
+
 module.exports = mongoose.model('User', userSchema);
 
 // const mongodb = require('mongodb');
