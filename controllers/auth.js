7d2cfaa914c048a99c54dc6eb09d0f61cf7ddfd2
k1yeon <gio13remanente@gmail.com>
2021-04-07 22:23:30 +0800

    fix: routing

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,6 @@
 exports.getLogin = (req, res, next) => {
-  res
-    .render('auth/login', {
-      pageTitle: 'Login',
-      path: '/login',
-      orders,
-    })
-    .catch(err => console.log(err));
+  res.render('auth/login', {
+    pageTitle: 'Login',
+    path: '/login',
+  });
 };
--- routes/auth.js
+++ routes/auth.js
@@ -4,6 +4,6 @@ const authController = require('../controllers/auth');
 
 const router = express.Router();
 
-router.get('/login', authController);
+router.get('/login', authController.getLogin);
 
 module.exports = router;
