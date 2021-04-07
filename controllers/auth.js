7d56e8fe81783978fbd4cfdc7607e91831126582
k1yeon <gio13remanente@gmail.com>
2021-04-08 03:21:01 +0800

    deleting cookies

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,5 +1,6 @@
+const User = require('../models/user');
+
 exports.getLogin = (req, res, next) => {
-  // const isLoggedIn = req.get('Cookie').split('=')[1] === 'true';
   console.log(req.session.isLoggedIn);
   res.render('auth/login', {
     pageTitle: 'Login',
@@ -9,6 +10,18 @@ exports.getLogin = (req, res, next) => {
 };
 
 exports.postLogin = (req, res, next) => {
-  req.session.isLoggedIn = true;
-  res.redirect('/');
+  User.findById('606a66c4ebb3a60f9881a092')
+    .then(user => {
+      req.session.isLoggedIn = true;
+      req.session.user = user;
+      res.redirect('/');
+    })
+    .catch(err => console.log(err));
+};
+
+exports.postLogout = (req, res, next) => {
+  req.session.destroy(err => {
+    console.log(err);
+    res.redirect('/');
+  });
 };
