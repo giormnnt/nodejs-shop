d6921728fa33e948024a190ddf7327b8b83c752e
k1yeon <gio13remanente@gmail.com>
2021-04-18 02:43:59 +0800

    adding pagination in products page

--- controllers/shop.js
+++ controllers/shop.js
@@ -17,12 +17,28 @@ const error500 = (err, next) => {
 exports.getProductList = (req, res, next) => {
   // * find method does not give cursor, it gives us products.
   // * if query large amounts of date turn this into cursor. (Products.find().cursor().next() or manipulate find to limit the set of data that is retrieved)
+  const page = +req.query.page || 1;
+  let totalItems;
+
   Product.find()
+    .countDocuments()
+    .then(numProducts => {
+      totalItems = numProducts;
+      return Product.find()
+        .skip((page - 1) * ITEMS_PER_PAGE)
+        .limit(ITEMS_PER_PAGE);
+    })
     .then(products => {
       res.render('shop/product-list', {
         products,
-        pageTitle: 'All Products!',
+        pageTitle: 'Products!',
         path: '/products',
+        currentPage: page,
+        hasNextPage: ITEMS_PER_PAGE * +page < totalItems,
+        hasPreviousPage: page > 1,
+        nextPage: page + 1,
+        previousPage: page - 1,
+        lastPage: Math.ceil(totalItems / ITEMS_PER_PAGE),
       });
     })
     .catch(err => {
