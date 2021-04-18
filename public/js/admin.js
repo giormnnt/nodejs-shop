43965f86421c3ef4524cab2822f1c00906021e61
k1yeon <gio13remanente@gmail.com>
2021-04-19 01:27:42 +0800

    manipulating DOM data

--- public/js/admin.js
+++ public/js/admin.js
@@ -3,13 +3,18 @@ const button = document.querySelector('.grid');
 const deleteProduct = btn => {
   const productId = btn.parentNode.querySelector('[name=productId]').value;
   const csrf = btn.parentNode.querySelector('[name=_csrf]').value;
+  const productElement = btn.closest('article');
 
   fetch(`/admin/product-list/${productId}`, {
     method: 'DELETE',
     headers: { 'csrf-token': csrf },
   })
     .then(result => {
-      console.log(result);
+      return result.json();
+    })
+    .then(data => {
+      console.log(data);
+      productElement.parentNode.removeChild(productElement);
     })
     .catch(err => {
       console.log(err);
